889a3a4db9bc71518039b71841c4307fd1915910
teleivo <teleivo@users.noreply.github.com>
2025-07-10 05:59:53 +0200

    var

--- plan-tracker-exporter-tests.sh
+++ plan-tracker-exporter-tests.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # Get the query plan for analysis
 
-mkdir plans
+set -u
+
+mkdir plans -p
 # relies on https://www.postgresql.org/docs/current/libpq-envars.html to connect to the DB
 psql -XqAt -f ./tracker-exporter-tests.sql > plans/"$PLAN_NAME".json
