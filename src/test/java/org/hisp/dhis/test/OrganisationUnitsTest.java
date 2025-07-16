bc9f369addd9d1f277ec13a4538cf4ad98f0b0e1
teleivo <teleivo@users.noreply.github.com>
2025-07-16 14:08:00 +0200

    use pre-built db image

--- src/test/java/org/hisp/dhis/test/OrganisationUnitsTest.java
+++ src/test/java/org/hisp/dhis/test/OrganisationUnitsTest.java
@@ -42,7 +42,7 @@ public class OrganisationUnitsTest extends Simulation {
   public OrganisationUnitsTest() {
     String baseUrl = System.getProperty("instance", "http://localhost:8080");
     String repeat = System.getProperty("repeat", "100");
-    // String pageSize = System.getProperty("pageSize");
+    String pageSize = System.getProperty("pageSize");
     // TODO maybe try this to see the effect on the response times
     // https://docs.gatling.io/concepts/scenario/#pace
     // String pause = System.getProperty("pause", "0");
@@ -82,9 +82,9 @@ public class OrganisationUnitsTest extends Simulation {
     // to be more accurate
     String query =
         "/api/organisationUnits?fields=:all,!name,!id,!favorites,!translations,!children,!sharing&pageSize=2000";
-    // if (pageSize != null) {
-    //
-    //     }
+    if (pageSize != null) {
+      query = "/api/organisationUnits?fields=:all,!name,!id,!favorites,!translations,!children,!sharing&pageSize=" + pageSize;
+    }
     ScenarioBuilder scenario =
         scenario(query)
             .repeat(Integer.parseInt(repeat))
